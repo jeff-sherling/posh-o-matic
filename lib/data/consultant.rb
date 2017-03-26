4dd4df4d3f1d0c743810730e184a892c8d8d83a1
jeff.sherling <jeff.sherling@perfectlyposh.com>
2017-03-26 01:35:29 -0600

    Changes to address Rubocop suggestions.

--- lib/data/consultant.rb
+++ lib/data/consultant.rb
@@ -1,81 +1,75 @@
 require 'user_factory'
 
+# Used for /join/signup form
 class Consultant
-  class << self
-
-    # Used for /join/signup form
-
-    def get_disagree_consultant
-      agree = get_valid_consultant
-      agree[:terms] = false
-      agree[:propay_terms] = false
-      agree
-    end
-
-    def get_minor_consultant
-      valid = get_valid_consultant
-      valid[:birth_date] = UserFactory.get_minor_birth_date
-      valid
-    end
-
-    def get_invalid_email
-      info = get_valid_consultant
-      info[:email] = UserFactory.get_invalid_email
-      info[:confirm_email] = "#{info[:email]}"
-      info
-    end
-
-    def get_mismatched_email
-      info = get_valid_consultant
-      info[:confirm_email] = "bad_#{info[:email]}"
-      info
-    end
-
-    def get_mismatched_password
-      info = get_valid_consultant
-      info[:confirm_password] = 'def456'
-      info
-    end
-
-    def get_valid_consultant
-      valid = get_address
-      valid[:sponsor_name] = 'Amanda Williams'
-      valid[:sponsor_zip] = '84101'
-      valid[:rep_site] = "#{valid[:first]}#{valid[:last]}"
-      valid[:propay_terms] = true
-      valid
-    end
-
-    def get_address
-      address = get_common
-      address[:address1] = UserFactory.get_address1
-      address[:address2] = UserFactory.get_address2
-      address[:city] = UserFactory.get_name
-      address[:state] = UserFactory.get_state
-      address[:zip] = UserFactory.get_zip_code
-      address[:terms] = true
-      address
-    end
-
-    def get_common
-      common = get_basic
-      common[:first] = UserFactory.get_name
-      common[:last] = UserFactory.get_name 12
-      common[:email] = UserFactory.get_email
-      common[:confirm_email] = "#{common[:email]}"
-      common[:password] = 'abc123'
-      common[:confirm_password] = common[:password]
-      common
-    end
-
-    def get_basic
-      basic = {}
-      basic[:ssn] = UserFactory.get_ssn
-      basic[:phone] = UserFactory.get_phone
-      basic[:birth_date] = UserFactory.get_birth_date
-      basic
-    end
-
+  def disagree_consultant
+    agree = valid_consultant
+    agree[:terms] = false
+    agree[:propay_terms] = false
+    agree
   end
 
+  def minor_consultant
+    valid = valid_consultant
+    valid[:birth_date] = UserFactory.get_minor_birth_date
+    valid
+  end
+
+  def invalid_email
+    info = valid_consultant
+    info[:email] = UserFactory.get_invalid_email
+    info[:confirm_email] = info[:email]
+    info
+  end
+
+  def mismatched_email
+    info = valid_consultant
+    info[:confirm_email] = "bad_#{info[:email]}"
+    info
+  end
+
+  def mismatched_password
+    info = valid_consultant
+    info[:confirm_password] = 'def456'
+    info
+  end
+
+  def valid_consultant
+    valid = address
+    valid[:sponsor_name] = 'Amanda Williams'
+    valid[:sponsor_zip] = '84101'
+    valid[:rep_site] = "#{valid[:first]}#{valid[:last]}"
+    valid[:propay_terms] = true
+    valid
+  end
+
+  def address
+    address = common
+    address[:address1] = UserFactory.get_address1
+    address[:address2] = UserFactory.get_address2
+    address[:city] = UserFactory.get_name
+    address[:state] = UserFactory.get_state
+    address[:zip] = UserFactory.get_zip_code
+    address[:terms] = true
+    address
+  end
+
+  def common
+    common = basic
+    common[:first] = UserFactory.get_name
+    common[:last] = UserFactory.get_name 12
+    common[:email] = UserFactory.get_email
+    common[:confirm_email] = common[:email]
+    common[:password] = 'abc123'
+    common[:confirm_password] = common[:password]
+    common
+  end
+
+  def basic
+    basic = {}
+    basic[:ssn] = UserFactory.get_ssn
+    basic[:phone] = UserFactory.get_phone
+    basic[:birth_date] = UserFactory.get_birth_date
+    basic
+  end
 end
--- test/unit/consultant_test.rb
+++ test/unit/consultant_test.rb
@@ -1,56 +1,60 @@
-$:.push '../../lib/data'
+$LOAD_PATH.push 'lib/data'
+$LOAD_PATH.push 'lib/log'
 require 'consultant'
 require 'minitest/autorun'
 require 'minitest/reporters'
-$:.push '../../lib/log'
 require 'console'
 
+# Consultant unit test.
 class ConsultantTest < Minitest::Test
-
   Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new,
                             Minitest::Reporters::JUnitReporter.new]
 
-  def test_get_basic
-    info = Consultant.get_basic
-    Console.log.info "Info: #{info}"
-    assert(info.size == 5, 'Hash should contain 5 elements')
+  def setup
+    @consultant = Consultant.new
   end
 
-  def test_get_common
-    info = Consultant.get_common
+  def test_basic
+    info = @consultant.basic
     Console.log.info "Info: #{info}"
-    assert(info.size == 11, 'Hash should contain 11 elements')
+    assert(info.size == 3, 'Hash should contain 3 elements')
   end
 
-  def test_get_address
-    info = Consultant.get_address
+  def test_common
+    info = @consultant.common
     Console.log.info "Info: #{info}"
-    assert(info.size == 17, 'Hash should contain 17 elements')
+    assert(info.size == 9, 'Hash should contain 9 elements')
   end
 
-  def test_get_valid
-    info = Consultant.get_valid_consultant
+  def test_address
+    info = @consultant.address
     Console.log.info "Info: #{info}"
-    assert(info.size == 21, 'Hash should contain 21 elements')
+    assert(info.size == 15, 'Hash should contain 15 elements')
   end
 
-  def test_get_mismatched_email
-    info = Consultant.get_mismatched_email
+  def test_valid_consultant
+    info = @consultant.valid_consultant
     Console.log.info "Info: #{info}"
-    assert(info[:email] != info[:confirm_email], 'Email addresses should NOT match.')
+    assert(info.size == 19, 'Hash should contain 19 elements')
   end
 
-  def test_get_mismatched_password
-    info = Consultant.get_mismatched_password
+  def test_mismatched_email
+    info = @consultant.mismatched_email
+    Console.log.info "Info: #{info}"
+    assert(info[:email] != info[:confirm_email],
+           'Email addresses should NOT match.')
+  end
+
+  def test_mismatched_password
+    info = @consultant.mismatched_password
     Console.log.info "Info: #{info}"
     assert(info[:password] != info[:confirm_password],
            'Passwords addresses should NOT match.')
   end
 
-  def test_get_minor
-    info = Consultant.get_minor_consultant
+  def test_minor
+    info = @consultant.minor_consultant
     Console.log.info "Info: #{info}"
-    assert(info.size == 21, 'Birth year should be 15 years ago.')
+    assert(info.size == 19, 'Minor hash should contain 19 elements.')
   end
-
 end
