82974bba406979adc6db553edeaf98d370b753e2
jeff.sherling <jeff.sherling@perfectlyposh.com>
2017-03-29 12:54:40 -0600

    New Password page/test update due to Rubocop

--- test/new_password_page_test.rb
+++ test/new_password_page_test.rb
@@ -1,13 +1,13 @@
-$:.push '../lib/page'
+$LOAD_PATH.push 'lib/page'
 require 'new_password_page'
 require 'login_page'
 require_relative 'base_test'
 
-class RequestNewPasswordTest < BaseTest
-
+# Test Request New Password page
+class NewPasswordPageTest < BaseTest
   def setup
     @driver = Selenium::WebDriver.for :firefox
-    @new_password = NewPasswordPage.new(@driver)
+    @new_password = NewPasswordPage.new@driver
   end
 
   def teardown
@@ -16,7 +16,8 @@ class RequestNewPasswordTest < BaseTest
 
   def test_error_when_submit_empty_field
     @new_password.error_request_password
-    assert(@new_password.username_error_present?, 'Username box error should be present.')
+    assert(@new_password.username_error_present?,
+           'Username box error should be present.')
   end
 
   def test_error_when_submit_invalid_username
@@ -28,5 +29,4 @@ class RequestNewPasswordTest < BaseTest
     login = @new_password.request_new_password('23425@mailinator.com')
     assert(login.status_alert_present?, 'Status alert should be present.')
   end
-
 end
