a9de5a83c03af5f26a56b58bad8d4fdf9702b36b
jeff.sherling <jeff.sherling@perfectlyposh.com>
2017-04-11 00:02:20 -0600

    Add Header methods, tests

--- lib/page/header.rb
+++ lib/page/header.rb
@@ -1,29 +1,95 @@
 require 'base_page'
+require 'products_page'
+require 'join_page'
+require 'perks_rewards_page'
+require 'company_about_page'
+require 'landing_page'
+require 'meet_the_team_page'
 
 # Header component (products, collections, etc.)
 class Header < BasePage
-  # Public locators
-  SITE_LOGO = { css: 'a.site-logo' }.freeze
+  PAGE_URL = '/'.freeze
 
-  PRODUCTS = { css: '' }.freeze
-  COLLECTIONS = { css: '' }.freeze
-  JOIN = { css: '' }.freeze
-  REWARDS = { css: '' }.freeze
-  ABOUT = { id: 'edit-submit-solr-search' }.freeze
+  # Selector strings
+  rewards_link = "#superfish-2 a.sf-depth-1[href$='/perks-rewards']"
+  posh_team_submenu = "#block-menu-menu-about-menu a[href$='/meettheteam']"
 
-  SEARCH_BTN = { css: '' }.freeze
+  # Locators
+  POSH_LOGO = { css: 'a.site-logo' }.freeze
+  PRODUCTS_LINK = { css: '#superfish-2 #shop-product-categories' }.freeze
+  SUBMENU = { css: '.views-row a' }.freeze
+  COLLECTIONS_LINK = { css: '#superfish-2 #shop-posh-collections' }.freeze
 
-  # Consultant locators
-  INCENTIVE = { css: '' }.freeze
-  MY_BUSINESS = { css: '' }.freeze
-  PERKS = { css: '' }.freeze
-  EVENTS = { css: '' }.freeze
-  ORDERS = { css: '' }.freeze
-  ACADEMY = { css: '' }.freeze
-  BOX = { css: '' }.freeze
+  JOIN_LINK = { css: "#superfish-2 a.sf-depth-1[href$='/join']" }.freeze
+  REWARDS_LINK = { css: rewards_link }.freeze
+  ABOUT_LINK = { css: '#superfish-2 #main-nav-about' }.freeze
+  POSH_TEAM_ABOUT_SUBMENU = { css: posh_team_submenu }.freeze
 
-  def initialize(driver)
+  SEARCH_BOX = { id: 'edit-search-api-views-fulltext' }.freeze
+  SEARCH_BTN = { id: 'edit-submit-solr-search' }.freeze
+
+  def initialize(driver, nav = true)
     super(driver)
-    wait_for { displayed?SITE_LOGO }
+    visit PAGE_URL if nav
+    wait_for { displayed?POSH_LOGO }
+  end
+
+  def click_posh_logo
+    click_on POSH_LOGO
+    LandingPage.new @driver, false
+  end
+
+  def click_products_link
+    click_on PRODUCTS_LINK
+    ProductsPage.new @driver, false
+  end
+
+  def click_collections_link
+    click_on COLLECTIONS_LINK
+    ProductsPage.new @driver, false
+  end
+
+  def click_join_link
+    click_on JOIN_LINK
+    JoinPage.new @driver, false
+  end
+
+  def click_rewards_link
+    click_on REWARDS_LINK
+    PerksRewardsPage.new @driver, false
+  end
+
+  def click_about_link
+    click_on ABOUT_LINK
+    CompanyAboutPage.new @driver, false
+  end
+
+  def click_about_posh_team_submenu
+    click_submenu ABOUT_LINK, POSH_TEAM_ABOUT_SUBMENU
+    MeetTheTeamPage.new @driver, false
+  end
+
+  def products_submenu_visible?
+    submenu_visible? PRODUCTS_LINK
+  end
+
+  def collections_submenu_visible?
+    submenu_visible? COLLECTIONS_LINK
+  end
+
+  def about_submenu_visible?
+    submenu_visible? ABOUT_LINK
+  end
+
+  private
+
+  def submenu_visible?(locator)
+    hover find locator
+    enabled? SUBMENU
+  end
+
+  def click_submenu(menu, submenu_item)
+    hover find menu
+    click_on submenu_item
   end
 end
