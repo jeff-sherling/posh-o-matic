b13fcc646f85fdaf3621c5ff3405478634549296
jeff.sherling <jeff.sherling@perfectlyposh.com>
2017-04-26 21:49:50 -0600

    Add Company About methods and tests

--- lib/page/company_about_page.rb
+++ lib/page/company_about_page.rb
@@ -9,19 +9,29 @@ class CompanyAboutPage < BasePage
   FOUNDED_HEADER = { css: '.founded-row h1.about-h1' }.freeze
   H1_HEADER = { css: 'h1.about-h1' }.freeze
   H2_HEADER = { css: 'h2' }.freeze
+  FOUNDERS_PHOTO = { css: '.hidden-sm .about-founders-photo img' }.freeze
+  FOUNDERS_CONTENT = { css: '.hidden-sm .about-founders-content p' }.freeze
 
   def initialize(driver, nav = true)
-    super(driver)
+    super driver
     visit(PAGE_URL) if nav
-    wait_for { displayed?(PAGE_HEADER) }
+    wait_for { displayed? PAGE_HEADER }
   end
 
   def pampering_brand_present?
-    displayed?(PAGE_HEADER)
+    displayed? PAGE_HEADER
   end
 
   def founded_header_present?
-    displayed?(FOUNDED_HEADER)
+    displayed? FOUNDED_HEADER
+  end
+
+  def founders_row_content_present?
+    displayed? FOUNDERS_CONTENT
+  end
+
+  def founders_photo_present?
+    displayed? FOUNDERS_PHOTO
   end
 
   def h2_header_count
--- test/company_about_page_test.rb
+++ test/company_about_page_test.rb
@@ -2,29 +2,34 @@ $LOAD_PATH.push 'lib/page'
 require_relative 'base_test'
 require 'company_about_page'
 
-# Test Company About page.
+# Test Company About page
 class CompanyAboutPageTest < BaseTest
   def setup
     @driver = Selenium::WebDriver.for :firefox
-    @about_page = CompanyAboutPage.new(@driver)
+    @about = CompanyAboutPage.new @driver
   end
 
   def teardown
     @driver.quit
   end
 
-  def test_required_h1_headings_are_present
-    assert(@about_page.pampering_brand_present? &&
-               @about_page.founded_header_present?,
-           'Pampering or Founded headers not present.')
-  end
-
-  def test_title_includes_about_the_company
-    assert(@about_page.page_title.include?('About the Company'),
-           'Title should include About the Company.')
+  def test_page_title
+    assert(@about.page_title.include?('About the Company'),
+           'Page title should contain About the Company')
   end
 
   def test_h2_headings_are_present
-    assert(@about_page.h2_header_count >= 5, 'Should be 5+ h2 headings.')
+    assert(@about.h2_header_count >= 5, 'Should be 5+ h2 headings.')
+  end
+
+  def test_h1_headings_are_present
+    assert(@about.pampering_brand_present? && @about.founded_header_present?,
+           'Pampering or Founded headers not present.')
+  end
+
+  def test_founders_row_elements_present
+    assert(@about.founders_photo_present? &&
+               @about.founders_row_content_present?,
+           'Founders content and photo should be present.')
   end
 end
